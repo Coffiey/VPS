2c19d0f578c84f3d512175ce36eaca8f4164ef68
Adam Burrough <adamjamesburrough@gmail.com>
2023-07-05 04:17:20 +0900

    immage added

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log(require("./assets/icon.png"));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Open up Adam to start working on your app!</Text>
+      <Image
+        fadeDuration={2000}
+        style={{ width: 50, height: 50 }}
+        source={require("./assets/d20.png")}
+        resizeMode='contain'
+      />
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
